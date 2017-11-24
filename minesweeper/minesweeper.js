a44b3b66de03e2e0edd5069784a7148910c7ee3c
paija poon <eisu.it.student04@gmail.com>
2017-11-24 03:22:56 +0000

    created 25 boxes

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -2,4 +2,13 @@
 
 $(function(){
     
+    var mine_sweeper = $('#game');
+    
+    
+    
+      $('#box1').click(function() {
+        $(this).css('background', '#ff0000');
+      });
 });
+    
+
